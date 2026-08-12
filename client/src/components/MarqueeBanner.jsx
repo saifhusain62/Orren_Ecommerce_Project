d50900216adc5fedabf00142606330e7cfe5a4f7
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-08-12 10:35:02 +0600

    marquee added

---
+++ client/src/components/MarqueeBanner.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const MarqueeBanner = () => {
+  const items = [
+    "SHORT PANTS FOR BEACH",
+    "COLORFUL TSHIRT",
+    "COLORFUL TSHIRT",
+    "COLORFUL TSHIRT",
+    "COLORFUL TSHIRT",
+    "SHORT PANTS FOR BEACH",
+    "COLORFUL TSHIRT",
+    "COLORFUL TSHIRT",
+  ];
+
+  return (
+    <div className="w-full bg-[#edf6fc] py-6 overflow-hidden flex items-center border-y border-blue-50 mt-10 mb-10">
+      <div className="flex whitespace-nowrap animate-marquee w-max">
+        {/* We duplicate the content twice to ensure a seamless infinite scroll */}
+        <div className="flex shrink-0 items-center justify-around w-[100vw] min-w-max">
+          {items.map((item, index) => (
+            <span key={index} className="text-xs font-bold tracking-[0.2em] text-[#1a202c] uppercase px-10">
+              {item}
+            </span>
+          ))}
+        </div>
+        <div className="flex shrink-0 items-center justify-around w-[100vw] min-w-max">
+          {items.map((item, index) => (
+            <span key={index} className="text-xs font-bold tracking-[0.2em] text-[#1a202c] uppercase px-10">
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MarqueeBanner
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import PromoBanners from '../components/PromoBanners'
 import NewArrivals from '../components/NewArrivals'
+import MarqueeBanner from '../components/MarqueeBanner'
 
 const Home = ({ category }) => {
   return (
@@ -9,6 +10,7 @@ const Home = ({ category }) => {
       <Hero category={category} />
       <PromoBanners />
       <NewArrivals />
+      <MarqueeBanner />
     </main>
   )
 }
