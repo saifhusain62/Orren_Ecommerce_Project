b92140b0bd78c91ac35b8cf016ef78a3d4a93242
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2026-08-13 12:08:59 +0600

    logo size chanegd

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -69,9 +69,9 @@ const Navbar = ({ category, setCategory }) => {
           </div>
         </div>
 
-        <div className="flex-1 flex justify-center">
+        <div className="flex-1 flex justify-start lg:justify-center">
           <Link to="/" className="flex flex-col items-center group">
-            <img src={logo} alt="Orren Logo" className="h-[40px] object-contain group-hover:scale-105 transition-transform" />
+            <img src={logo} alt="Orren Logo" className="h-[28px] lg:h-[40px] object-contain group-hover:scale-105 transition-transform" />
           </Link>
         </div>
 
